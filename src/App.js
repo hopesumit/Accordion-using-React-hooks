6cdb5bb0cb1d5b9d3758a2916bd9937bfbc3e6bb
Sumit Kashyap <sumit@digithinkit.com>
2020-01-06 23:24:35 +0530

    accordion animated app using react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css'
 
-import React, {Component} from 'react'
+import React from 'react'
+import Accordion from "./component/Accordion"
 
-class App extends Component {
-  render() {
-    return <div className="App">
-      <div className="App-heading App-flex">
-        <h2>Welcome to <span className="App-react">React</span></h2>
-      </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={require('./react.svg')}/>
-        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-      </div>
+function App () {
+  
+    return (
+     <div className="App">
+    <Accordion title="What is your return policy ?" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " />
+    <Accordion title="This is awesome ?" 
+    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua " />
+    <Accordion title="How to earn most in it ?" 
+    content="
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    </br>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    </br>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+    " />
     </div>
-  }
+    )
 }
 
 export default App
